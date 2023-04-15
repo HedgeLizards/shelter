502b0961b8542a74b8331534e64d534cec582589
troido <troido@protonmail.com>
2023-04-15 13:48:20 +0200

    include environment script

---
+++ scripts/environment.gd
@@ -0,0 +1,9 @@
+extends WorldEnvironment
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var moon_position = %Moon.quaternion * Vector3(0,0,1)
+	environment.sky.sky_material.set_shader_parameter("moon_position", moon_position)
+
+
